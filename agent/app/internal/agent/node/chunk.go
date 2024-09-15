298ce5fa224fadda66d9c6711f591a6eb244714a
Wojciech Suszko <wsuszko14@gmail.com>
2024-09-15 15:45:06 +0200

    refactor

--- agent/app/internal/agent/node/chunk.go
+++ agent/app/internal/agent/node/chunk.go
@@ -3,6 +3,7 @@ package node
 type Chunk struct {
 	Cluster   string `json:"cluster"`
 	Kind      string `json:"kind"`
+	Timestamp int64  `json:"timestamp"`
 	Name      string `json:"name"`
 	Namespace string `json:"namespace"`
 	Content   string `json:"content"`
