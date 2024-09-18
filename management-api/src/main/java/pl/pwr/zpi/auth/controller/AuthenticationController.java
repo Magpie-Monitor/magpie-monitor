ddc8dd14209cff201e6f38348dbeed01262afabe
MDybek <dybek8780@gmail.com>
2024-09-18 20:47:42 +0200

    refactor

--- management-api/src/main/java/pl/pwr/zpi/auth/controller/AuthenticationController.java
+++ management-api/src/main/java/pl/pwr/zpi/auth/controller/AuthenticationController.java
@@ -3,11 +3,9 @@ package pl.pwr.zpi.auth.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import pl.pwr.zpi.auth.dto.ErrorMessageDTO;
 import pl.pwr.zpi.auth.service.AuthenticationService;
 
 import java.security.Principal;
-
 
 @RestController
 @RequiredArgsConstructor
@@ -17,19 +15,11 @@ public class AuthenticationController {
 
     @GetMapping("/api/v1/auth/user-details")
     public ResponseEntity<?> getUser(Principal principal) {
-        try {
-            return ResponseEntity.ok().body(service.getUserDetails(principal.getName()));
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(new ErrorMessageDTO(e.getMessage()));
-        }
+        return ResponseEntity.ok().body(service.getUserDetails(principal.getName()));
     }
 
     @GetMapping("/api/vi/auth/token-validation-time")
     public ResponseEntity<?> getTokenValidationTime(@CookieValue("authToken") String authToken) {
-        try {
-            return ResponseEntity.ok().body(service.getTokenValidationTime(authToken));
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(new ErrorMessageDTO(e.getMessage()));
-        }
+        return ResponseEntity.ok().body(service.getTokenValidationTime(authToken));
     }
 }
