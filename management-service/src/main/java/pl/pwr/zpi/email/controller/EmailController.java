fd8bcde7feef53dac99f1767e626614840ef1782
MDybek <dybek8780@gmail.com>
2024-10-03 22:13:48 +0200

    management-service: CRUD for emails + minor fixes

--- management-service/src/main/java/pl/pwr/zpi/email/controller/EmailController.java
+++ management-service/src/main/java/pl/pwr/zpi/email/controller/EmailController.java
@@ -1,7 +1,11 @@
 package pl.pwr.zpi.email.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -9,9 +13,25 @@ import org.springframework.web.bind.annotation.*;
 public class EmailController {
     private final EmailService emailService;
 
-    @GetMapping("/test-notification")
-    public void sendTestEmail(@RequestParam String receiverEmail) {
-        emailService.sendTestEmail(receiverEmail);
+    @GetMapping
+    public ResponseEntity<List<EmailReceiver>> getAllEmails() {
+        return ResponseEntity.ok().body(emailService.getAllEmails());
+    }
+
+    @PostMapping
+    public ResponseEntity<EmailReceiver> addEmail(@Valid @RequestBody EmailReceiverDTO emailReceiver) {
+        emailService.addNewEmail(emailReceiver);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<EmailReceiver> updateEmail(@PathVariable Long id, @Valid @RequestBody EmailReceiverDTO emailReceiver) {
+        emailService.updateEmail(id, emailReceiver);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}/test-notification")
+    public void sendTestEmail(@PathVariable Long id) {
+        emailService.sendTestEmail(id);
     }
 }
-
