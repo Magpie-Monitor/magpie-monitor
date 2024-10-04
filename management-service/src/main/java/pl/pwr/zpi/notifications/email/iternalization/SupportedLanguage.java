44e08d1db7b37aa2859f049a6cdb7081692bc464
MDybek <dybek8780@gmail.com>
2024-10-04 23:29:07 +0200

    management-service: Slack notifications implemented + small refactor
    (cherry picked from commit 859226b00b385bb544a6f1048da693517017fb4a)

---
+++ management-service/src/main/java/pl/pwr/zpi/notifications/email/iternalization/SupportedLanguage.java
@@ -0,0 +1,25 @@
+package pl.pwr.zpi.notifications.email.iternalization;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Optional;
+
+@Getter
+public enum SupportedLanguage {
+
+    EN(Locale.of("en"));
+
+    private final Locale locale;
+
+    SupportedLanguage(Locale locale) {
+        this.locale = locale;
+    }
+
+    public static Optional<SupportedLanguage> fromLocale(Locale locale) {
+        return Arrays.stream(SupportedLanguage.values())
+                .filter(language -> language.getLocale().getLanguage().equals(locale.getLanguage()))
+                .findFirst();
+    }
+}
