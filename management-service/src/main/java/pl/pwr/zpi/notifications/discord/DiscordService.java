3d917e037a561ffa4e44154400ecd4b46ea26a10
MDybek <dybek8780@gmail.com>
2024-10-05 23:39:52 +0200

    management-service: Code formatting

--- management-service/src/main/java/pl/pwr/zpi/notifications/discord/DiscordService.java
+++ management-service/src/main/java/pl/pwr/zpi/notifications/discord/DiscordService.java
@@ -1,8 +1,5 @@
 package pl.pwr.zpi.notifications.discord;
 
-import lombok.SneakyThrows;
-
 public interface DiscordService {
-    @SneakyThrows
     void sendMessage(String message, String webhookUrl);
 }
