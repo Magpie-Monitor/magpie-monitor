b2d775d4bc4131cf9470bf6ef4b8cec665e4f479
Wojciech Suszko <wsuszko14@gmail.com>
2024-10-06 14:22:53 +0200

    agent: Add ClusterName to NodeState struct

--- agent/app/internal/agent/node/data/node_state.go
+++ agent/app/internal/agent/node/data/node_state.go
@@ -3,13 +3,14 @@ package data
 import "time"
 
 type NodeState struct {
+	ClusterName   string   `json:"clusterName"`
 	NodeName      string   `json:"nodeName"`
 	CollectedAtMs int64    `json:"collectedAtMs"`
 	WatchedFiles  []string `json:"watchedFiles"`
 }
 
-func NewNodeState(nodeName string, watchedFiles []string) NodeState {
-	return NodeState{NodeName: nodeName, WatchedFiles: watchedFiles}
+func NewNodeState(clusterName, nodeName string, watchedFiles []string) NodeState {
+	return NodeState{ClusterName: clusterName, NodeName: nodeName, WatchedFiles: watchedFiles}
 }
 
 func (n *NodeState) SetTimestamp() {
