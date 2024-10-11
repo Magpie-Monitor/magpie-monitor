6afd2193f434e8b25dc77ce08c9b7a0b6e4395ff
Wieslaw <al.lo3opole@gmail.com>
2024-10-11 14:12:43 +0200

    client: Add table component

--- client/src/pages/Router.tsx
+++ client/src/pages/Router.tsx
@@ -1,6 +1,6 @@
 import { Route, createBrowserRouter, createRoutesFromElements, defer } from 'react-router-dom';
 import Login from './Login/Login';
-import { ProtectedLayout } from 'providers/AuthProvider/ProtectedLayout';
+// import { ProtectedLayout } from 'providers/AuthProvider/ProtectedLayout';
 import { AuthLayout } from 'providers/AuthProvider/AuthLayout';
 import Home from './Home/Home';
 import { getAuthInfo } from 'providers/AuthProvider/AuthProvider';
@@ -14,14 +14,15 @@ const router = createBrowserRouter(
           authData: getAuthInfo(),
         });
       }}
-      errorElement={<Login />}
+      errorElement={<Home />}
     >
       <Route path="/login" element={<Login />} />
-      <Route path="/" element={<ProtectedLayout />}>
-        <Route path="/" element={<Home />} />
-      </Route>
+      <Route path="/" element={<Home />} />
     </Route>,
   ),
 );
+
+// <Route path="/" element={<ProtectedLayout />}>
+// </Route>
 
 export default router;
