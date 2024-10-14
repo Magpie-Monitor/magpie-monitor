59bd6fd4c6625b52336040565e9bd087f3bae67b
Wieslaw <al.lo3opole@gmail.com>
2024-10-14 18:39:41 +0200

    client: Add style parameters to Table component

--- client/src/components/Table/Table.tsx
+++ client/src/components/Table/Table.tsx
@@ -7,6 +7,21 @@ interface TableRow {
 export interface TableProps<Row extends TableRow> {
   columns: TableColumn<Row>[];
   rows: Row[];
+
+  /**
+   * Max height of the table defined in the css units (px, rem, ect.)
+   * Used to accomodate sticky table headers.
+   * By default table doesn't have a max height.
+   */
+  maxHeight?: string;
+
+  /**
+   * AlignLeft forces all columns to take as little space as possible
+   * making all columns tightly packed on the left side of the table.
+   * Useful if we need a column that is always the size of its contents.
+   * Default value is false
+   */
+  alignLeft?: boolean;
 }
 
 export interface TableColumn<Row extends TableRow> {
@@ -17,19 +32,33 @@ export interface TableColumn<Row extends TableRow> {
 export interface TableRowProps<Row extends TableRow> {
   row: Row;
   columns: TableColumn<Row>[];
+  alignLeft: boolean;
 }
 
 export const getCellRowSpan = <Row extends TableRow>(
   columns: TableColumn<Row>[],
+  alignLeft: boolean,
 ): { gridColumn: string } => {
-  return { gridColumn: `span ${columns.length}` };
+  if (columns.length == 0 || alignLeft) {
+    return { gridColumn: '' };
+  }
+
+  return { gridColumn: `span ${Math.floor(100 / columns.length)}` };
 };
 
-function TableBodyRow<Row extends TableRow>({ row, columns }: TableRowProps<Row>): React.ReactNode {
+function getTableStyle(maxHeight: string) {
+  return { maxHeight };
+}
+
+function TableBodyRow<Row extends TableRow>({
+  row,
+  columns,
+  alignLeft,
+}: TableRowProps<Row>): React.ReactNode {
   return (
     <div className="table__row">
       {columns.map((column: TableColumn<Row>) => (
-        <div key="index" className={'table__cell'}>
+        <div key="index" className={'table__cell'} style={getCellRowSpan(columns, alignLeft)}>
           {column.customComponent ? column.customComponent(row) : row[column.columnKey]}
         </div>
       ))}
@@ -37,19 +66,26 @@ function TableBodyRow<Row extends TableRow>({ row, columns }: TableRowProps<Row>
   );
 }
 
-function Table<T extends TableRow>({ columns, rows }: TableProps<T>): React.ReactNode {
+function Table<T extends TableRow>({
+  columns,
+  rows,
+  maxHeight,
+  alignLeft = false,
+}: TableProps<T>): React.ReactNode {
+  const tableStyle = maxHeight ? getTableStyle(maxHeight) : {};
+
   return (
     <div className="table">
       <div className="table__headers">
         {columns.map((column: TableColumn<T>, index: number) => (
-          <div key={index} className="table__header">
+          <div key={index} className="table__header" style={getCellRowSpan(columns, alignLeft)}>
             {column.header}
           </div>
         ))}
       </div>
-      <div className="table__body">
+      <div className="table__body" style={tableStyle}>
         {rows.map((row: T, index: number) => (
-          <TableBodyRow key={index} row={row} columns={columns} />
+          <TableBodyRow key={index} row={row} columns={columns} alignLeft={alignLeft} />
         ))}
       </div>
     </div>
