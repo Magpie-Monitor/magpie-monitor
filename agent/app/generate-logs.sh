a0a48ea2cfabda80fb93d5c37dc5b4ea66c52371
Wojciech Suszko <wsuszko14@gmail.com>
2024-10-18 14:09:06 +0200

    agent: Introduce script for journalctl log streaming.

--- agent/app/generate-logs.sh
+++
@@ -1,8 +0,0 @@
-for i in {1..1000000};
-do
-  echo "testwetwerwedwxsxwsxwsxwxw$i" >> test.log
-#	echo $(tr -dc A-Za-z0-9 </dev/urandom | head -c 13) >> test.log
-#	echo $(tr -dc A-Za-z0-9 </dev/urandom | head -c 13) >> test2.log
-	sleep 1
-done
-
