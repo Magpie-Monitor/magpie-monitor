0fb331c73623e68d71ac7127049e05388611f18a
Wojciech Suszko <wsuszko14@gmail.com>
2024-10-18 22:32:48 +0200

    management-service: Improve incident endpoints, refactor

--- management-service/src/main/java/pl/pwr/zpi/reports/dto/report/node/NodeReport.java
+++ management-service/src/main/java/pl/pwr/zpi/reports/dto/report/node/NodeReport.java
@@ -1,7 +1,10 @@
 package pl.pwr.zpi.reports.dto.report.node;
 
+import java.util.List;
+
 public record NodeReport(
         String node,
         String precision,
-        String customPrompt
+        String customPrompt,
+        List<NodeIncident> nodeIncidents
 ) {}
