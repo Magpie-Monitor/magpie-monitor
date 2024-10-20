67bb0673ce8f113195193342f909deac493b649e
Wojciech Suszko <wsuszko14@gmail.com>
2024-10-20 14:22:12 +0200

    management-service, go metadata: Introduce e2e communication between management-service and metadata-service.

--- go/services/cluster_metadata/pkg/services/event_service.go
+++ go/services/cluster_metadata/pkg/services/event_service.go
@@ -2,70 +2,72 @@ package services
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 
-	kafka "github.com/Magpie-Monitor/magpie-monitor/pkg/kafka"
+	sharedkafka "github.com/Magpie-Monitor/magpie-monitor/pkg/kafka"
 	"github.com/Magpie-Monitor/magpie-monitor/services/cluster_metadata/pkg/repositories"
 	"github.com/google/uuid"
 	"go.uber.org/zap"
 )
 
-func NewEventEmitter(log *zap.Logger, credentials *kafka.KafkaCredentials) *EventEmitter {
+func NewEventEmitter(log *zap.Logger, credentials *sharedkafka.KafkaCredentials) *EventEmitter {
 	return &EventEmitter{log: log, applicationMetadataWriter: NewApplicationMetadataStreamWriter(credentials), nodeMetadataWriter: NewNodeMetadataStreamWriter(credentials)}
 }
 
-func NewApplicationMetadataStreamWriter(credentials *kafka.KafkaCredentials) *kafka.StreamWriter {
+func NewApplicationMetadataStreamWriter(credentials *sharedkafka.KafkaCredentials) *sharedkafka.StreamWriter {
 	appTopic, ok := os.LookupEnv("CLUSTER_METADATA_APPLICATION_TOPIC")
 	if !ok {
 		panic("CLUSTER_METADATA_APPLICATION_TOPIC env variable not provided")
 	}
 
-	return kafka.NewStreamWriter(credentials, appTopic, 0)
+	return sharedkafka.NewStreamWriter(credentials, appTopic, 0)
 }
 
-func NewNodeMetadataStreamWriter(credentials *kafka.KafkaCredentials) *kafka.StreamWriter {
+func NewNodeMetadataStreamWriter(credentials *sharedkafka.KafkaCredentials) *sharedkafka.StreamWriter {
 	nodeTopic, ok := os.LookupEnv("CLUSTER_METADATA_NODE_TOPIC")
 	if !ok {
 		panic("CLUSTER_METADATA_NODE_TOPIC env variable not provided")
 	}
 
-	return kafka.NewStreamWriter(credentials, nodeTopic, 0)
+	return sharedkafka.NewStreamWriter(credentials, nodeTopic, 0)
 }
 
 type ApplicationMetadataUpdated struct {
-	requestId string
-	metadata  repositories.AggregatedApplicationMetadata
+	RequestId string
+	Metadata  repositories.AggregatedApplicationMetadata
 }
 
 type NodeMetadataUpdated struct {
-	requestId string
-	metadata  repositories.AggregatedNodeMetadata
+	RequestId string
+	Metadata  repositories.AggregatedNodeMetadata
 }
 
 type EventEmitter struct {
 	log                       *zap.Logger
-	applicationMetadataWriter *kafka.StreamWriter
-	nodeMetadataWriter        *kafka.StreamWriter
+	applicationMetadataWriter *sharedkafka.StreamWriter
+	nodeMetadataWriter        *sharedkafka.StreamWriter
 }
 
 func (e *EventEmitter) EmitApplicationMetadataUpdatedEvent(metadata repositories.AggregatedApplicationMetadata) error {
-	event := ApplicationMetadataUpdated{requestId: uuid.New().String(), metadata: metadata}
+	event := ApplicationMetadataUpdated{RequestId: uuid.New().String(), Metadata: metadata}
 	return e.emitEvent(event, e.applicationMetadataWriter)
 }
 
 func (e *EventEmitter) EmitNodeMetadataUpdatedEvent(metadata repositories.AggregatedNodeMetadata) error {
-	event := NodeMetadataUpdated{requestId: uuid.New().String(), metadata: metadata}
+	fmt.Println("metadata emitted:", metadata)
+	event := NodeMetadataUpdated{RequestId: uuid.New().String(), Metadata: metadata}
 	return e.emitEvent(event, e.nodeMetadataWriter)
 }
 
-func (e *EventEmitter) emitEvent(event interface{}, writer *kafka.StreamWriter) error {
-	e.log.Info("Emitting an event:", zap.Any("event", event))
-
+func (e *EventEmitter) emitEvent(event interface{}, writer *sharedkafka.StreamWriter) error {
 	jsonEvent, err := json.Marshal(&event)
 	if err != nil {
 		e.log.Error("Error converting metadata event to JSON", zap.Error(err))
 		return err
 	}
+
+	fmt.Println("jsonEvent:", string(jsonEvent))
 
 	writer.Write(string(jsonEvent))
 	return nil
