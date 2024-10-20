ac8c9a62ef48b62928cef4fed58b05c887b981a0
Nikodem <nikodem.swierkowski@gmail.com>
2024-10-20 14:48:38 +0200

    client: add tag button and action button

---
+++ client/src/components/ActionButton/ActionButton.tsx
@@ -0,0 +1,27 @@
+import './ActionButton.scss';
+
+export enum ActionButtonColor {
+  GREEN,
+  RED,
+}
+
+export interface ActionButtonProps {
+  onClick: () => void;
+  description: string;
+  color: ActionButtonColor;
+}
+
+const actionButtonColorToClass = {
+    [ActionButtonColor.GREEN]: 'action-button--green',
+    [ActionButtonColor.RED]: 'action-button--red',
+};
+
+const ActionButton = ({ onClick, description, color }: ActionButtonProps) => {
+  return (
+    <button onClick={onClick} className={actionButtonColorToClass[color]}>
+      {description}
+    </button>
+  );
+};
+
+export default ActionButton;
