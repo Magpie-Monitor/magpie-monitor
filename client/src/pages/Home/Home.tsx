e58fdb97cc61a16147483221ff7f94d0c206efda
MDybek <dybek8780@gmail.com>
2024-10-21 23:38:17 +0200

    client: refactor

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,24 +1,29 @@
 import ImportantFindings from './components/ImportantFindings';
 import PlaceholderComponent from '@/components/PlaceholderComponent/PlaceholderComponent.tsx';
 import './Home.scss';
-import PlaceholderComponentTitle from '@/components/PlaceholderComponent/PlaceholderComponentTitle/PlaceholderComponentTitle.tsx';
+import PlaceholderComponentTitle
+    from '@/components/PlaceholderComponent/PlaceholderComponentTitle/PlaceholderComponentTitle.tsx';
 import ScanStats from './components/ScanStats';
+
+//panel name should be aligned to the left of the content
 
 const Home = () => {
     return (
         <div className="home">
             <div className="home__content">
-                <p className="home__content__heading">Dashboard</p>
                 <div>
-                    <PlaceholderComponent
-                        icon={'setting-icon'}
-                        title={<PlaceholderComponentTitle source="production-services"
-                                startTime="19.04.2023" endTime="25.04.2023"/>}>
-                        <div>
-                            <ScanStats/>
-                            <ImportantFindings/>
-                        </div>
-                    </PlaceholderComponent>
+                    <p className="home__content__heading">Dashboard</p>
+                    <div>
+                        <PlaceholderComponent
+                            icon={'setting-icon'}
+                            title={<PlaceholderComponentTitle source="production-services"
+                                                              startTime="19.04.2023" endTime="25.04.2023"/>}>
+                            <div>
+                                <ScanStats/>
+                                <ImportantFindings/>
+                            </div>
+                        </PlaceholderComponent>
+                    </div>
                 </div>
             </div>
         </div>
