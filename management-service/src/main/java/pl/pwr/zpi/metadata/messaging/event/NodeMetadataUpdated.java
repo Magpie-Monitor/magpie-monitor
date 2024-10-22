a09bbc69f20d20610f4b0d67337bd588a5b36ac8
Wojciech Suszko <wsuszko14@gmail.com>
2024-10-22 15:57:51 +0200

    management-service: Introduce mongodb for management-service metadata events.

--- management-service/src/main/java/pl/pwr/zpi/metadata/messaging/event/NodeMetadataUpdated.java
+++ management-service/src/main/java/pl/pwr/zpi/metadata/messaging/event/NodeMetadataUpdated.java
@@ -2,7 +2,5 @@ package pl.pwr.zpi.metadata.messaging.event;
 
 import pl.pwr.zpi.metadata.messaging.event.node.AggregatedNodeMetadata;
 
-import java.util.List;
-
-public record NodeMetadataUpdated(String requestId, List<AggregatedNodeMetadata> metadata) {
+public record NodeMetadataUpdated(String requestId, AggregatedNodeMetadata metadata) {
 }
