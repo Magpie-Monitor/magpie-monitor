0da7fc122b2d55564076a311e87345c180f874b5
Wojciech Suszko <wsuszko14@gmail.com>
2024-10-23 19:52:15 +0200

    management-service: Remove channelName from SlackChannel DTO.

--- management-service/src/main/java/pl/pwr/zpi/notifications/SlackChannel.java
+++ management-service/src/main/java/pl/pwr/zpi/notifications/SlackChannel.java
@@ -7,16 +7,13 @@ import java.time.LocalDateTime;
 public record SlackChannel(
         String name,
         String server,
-        String channelName,
         LocalDateTime updatedAt,
         LocalDateTime createdAt) {
 
-    // TODO - receiver should have channel configured
     public static SlackChannel of(SlackReceiver receiver) {
         return new SlackChannel(
                 receiver.getReceiverName(),
                 receiver.getWebhookUrl(),
-                "",
                 receiver.getUpdatedAt(),
                 receiver.getCreatedAt()
         );
