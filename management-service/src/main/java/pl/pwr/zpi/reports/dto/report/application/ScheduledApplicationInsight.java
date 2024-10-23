c83fc5e136fa9910d52cece920c0cc7ecee880df
Wojciech Suszko <wsuszko14@gmail.com>
2024-10-23 19:50:19 +0200

    docker, management-service: Align ScheduledApplicationInsights and ScheduledNodeInsights with new reports-service contract, move comment do appropriate compose.

--- management-service/src/main/java/pl/pwr/zpi/reports/dto/report/application/ScheduledApplicationInsight.java
+++ management-service/src/main/java/pl/pwr/zpi/reports/dto/report/application/ScheduledApplicationInsight.java
@@ -3,7 +3,7 @@ package pl.pwr.zpi.reports.dto.report.application;
 import java.util.List;
 
 public record ScheduledApplicationInsight(
-        String id,
+        List<String> scheduledJobIds,
         Long sinceMs,
         Long toMs,
         String clusterId,
