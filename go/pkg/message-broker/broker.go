7c8b78f9da0b6e7072ae0d33aca2be9ff2b80464
Wieslaw <al.lo3opole@gmail.com>
2024-10-28 17:20:07 +0100

    docker-compose.yml: Add proxy config for kafka-ui

--- go/pkg/message-broker/broker.go
+++ go/pkg/message-broker/broker.go
@@ -2,5 +2,5 @@ package messagebroker
 
 type MessageBroker[T any] interface {
 	Publish(key string, message T) error
-	Subscribe(ch chan<- T, errCh chan<- error)
+	Subscribe(messages chan<- T, errors chan<- error)
 }
