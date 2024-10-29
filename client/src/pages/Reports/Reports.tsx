97c4f402c8be68ba1391eef44174e126f6969f24
Wieslaw <al.lo3opole@gmail.com>
2024-10-29 22:50:43 +0100

    client: Migrate Reports page to PageTemplate

--- client/src/pages/Reports/Reports.tsx
+++ client/src/pages/Reports/Reports.tsx
@@ -3,95 +3,96 @@ import SectionComponent from 'components/SectionComponent/SectionComponent.tsx';
 import Table, { TableColumn } from 'components/Table/Table.tsx';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { ManagmentServiceApiInstance, ReportSummary } from 'api/managment-service';
+import {
+  ManagmentServiceApiInstance,
+  ReportSummary,
+} from 'api/managment-service';
 import SVGIcon from 'components/SVGIcon/SVGIcon.tsx';
 import UrgencyBadge from 'components/UrgencyBadge/UrgencyBadge.tsx';
+import PageTemplate from 'components/PageTemplate/PageTemplate';
+import HeaderWithIcon from 'components/PageTemplate/components/HeaderWithIcon/HeaderWithIcon';
 
 const Reports = () => {
-    const [rows, setRows] = useState<ReportSummary[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const navigate = useNavigate();
+  const [rows, setRows] = useState<ReportSummary[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const navigate = useNavigate();
 
-    const handleRowClick = (id: string) => {
-        navigate(`/reports/${id}`);
-    };
+  const handleRowClick = (id: string) => {
+    navigate(`/reports/${id}`);
+  };
 
-    const handleAddNewReport = () => {
-        navigate('/reports/new');
-    };
+  const handleAddNewReport = () => {
+    navigate('/reports/new');
+  };
 
-    const columns: Array<TableColumn<ReportSummary>> = [
-        {
-            header: 'Cluster',
-            columnKey: 'clusterId',
-            customComponent: (row: ReportSummary) => (
-                <a className='reports__content__link' href="#" onClick={() => handleRowClick(row.id)}>
-                    {row.clusterId}
-                </a>
-            )
-        },
-        { header: 'Title', columnKey: 'title' },
-        {
-            header: 'Urgency',
-            columnKey: 'urgency',
-            customComponent: (row: ReportSummary) => <UrgencyBadge label={row.urgency} />
-        },
-        { header: 'Start date', columnKey: 'startDate' },
-        { header: 'End date', columnKey: 'endDate' }
-    ];
-
-    const fetchReports = async () => {
-        try {
-            const reports = await ManagmentServiceApiInstance.getReports();
-            const mappedReports = reports.map((report: ReportSummary) => ({
-                ...report,
-                startDate: new Date(report.sinceMs).toLocaleString(),
-                endDate: new Date(report.toMs).toLocaleString(),
-            }));
-            setRows(mappedReports);
-        } catch (error) {
-            console.error('Error fetching reports:', error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        fetchReports();
-    }, []);
-
-    return (
-        <div className="reports">
-            <div className="reports__content">
-                <div>
-                    <div className='reports__content__heading'>
-                        <SVGIcon iconName='reports-list-icon' />
-                        <p className="reports__content__heading__paragraph">Reports</p>
-                    </div>
-                    <div className="reports__content__dashboard">
-                        <SectionComponent
-                            icon={'chart-icon'}
-                            title={'Weekly reports'}
-                            callback={handleAddNewReport}
-                        >
-                            <div className="reports__content__dashboard__content">
-                                {loading ? (
-                                    <p className="reports__content__dashboard__content__paragraph">Loading...</p>
-                                ) : rows.length === 0 ? (
-                                    <p className="reports__content__dashboard__content__paragraph">No reports. Generate new report (TBA: link)</p>
-                                ) : (
-                                    <Table
-                                        columns={columns}
-                                        rows={rows}
-                                    />
-                                )}
-                            </div>
-                        </SectionComponent>
-                    </div>
-                </div>
-            </div>
+  const columns: Array<TableColumn<ReportSummary>> = [
+    {
+      header: 'Cluster',
+      columnKey: 'clusterId',
+      customComponent: (row: ReportSummary) => (
+        <div className="reports__link" onClick={() => handleRowClick(row.id)}>
+          {row.clusterId}
         </div>
-    );
+      ),
+    },
+    { header: 'Title', columnKey: 'title' },
+    {
+      header: 'Urgency',
+      columnKey: 'urgency',
+      customComponent: (row: ReportSummary) => (
+        <UrgencyBadge label={row.urgency} />
+      ),
+    },
+    { header: 'Start date', columnKey: 'startDate' },
+    { header: 'End date', columnKey: 'endDate' },
+  ];
+
+  const fetchReports = async () => {
+    try {
+      const reports = await ManagmentServiceApiInstance.getReports();
+      const mappedReports = reports.map((report: ReportSummary) => ({
+        ...report,
+        startDate: new Date(report.sinceMs).toLocaleString(),
+        endDate: new Date(report.toMs).toLocaleString(),
+      }));
+      setRows(mappedReports);
+    } catch (error) {
+      console.error('Error fetching reports:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchReports();
+  }, []);
+
+  return (
+    <PageTemplate
+      header={
+        <HeaderWithIcon
+          title={'Reports'}
+          icon={<SVGIcon iconName="reports-list-icon" />}
+        />
+      }
+    >
+      <SectionComponent
+        icon={'chart-icon'}
+        title={'Weekly reports'}
+        callback={handleAddNewReport}
+      >
+        {loading ? (
+          <div className="reports__no-data">Loading...</div>
+        ) : rows.length === 0 ? (
+          <div className="reports__no-data">
+            No reports. Generate new report (TBA: link)
+          </div>
+        ) : (
+          <Table columns={columns} rows={rows} />
+        )}
+      </SectionComponent>
+    </PageTemplate>
+  );
 };
 
 export default Reports;
