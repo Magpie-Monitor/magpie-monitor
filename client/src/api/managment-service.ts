bf463eeb1b1838f64b9bdac22fa0f938f04bfba4
Wieslaw <al.lo3opole@gmail.com>
2024-10-29 22:49:22 +0100

    client: Add mock GET/clusters endpoint

--- client/src/api/managment-service.ts
+++ client/src/api/managment-service.ts
@@ -19,6 +19,28 @@ export interface ReportSummary {
   [key: string]: string | number;
 }
 
+export interface ClusterSummary {
+  id: string;
+  isRunning: boolean;
+  precision: 'HIGH' | 'MEDIUM' | 'LOW';
+  updatedAt: number;
+  slackChannels: {
+    name: string;
+    webhookUrl: string;
+    updatedAt: number;
+  }[];
+  discordChannels: {
+    name: string;
+    webhookUrl: string;
+    updatedAt: number;
+  }[];
+  mailChannels: {
+    name: string;
+    email: string;
+    updatedAt: number;
+  }[];
+}
+
 const MANAGMENT_SERVICE_URL = import.meta.env.VITE_BACKEND_URL;
 const VALID_URGENCY_VALUES: ReportSummary['urgency'][] = [
   'HIGH',
@@ -100,12 +122,79 @@ class ManagmentServiceApi {
     reports.forEach((report) => {
       if (!VALID_URGENCY_VALUES.includes(report.urgency)) {
         throw new Error(
-          `Invalid urgency value "${report.urgency}" for report ID ${report.id}. Allowed values are: ${VALID_URGENCY_VALUES.join(', ')}`,
+          `Invalid urgency value "${report.urgency}" for report ID 
+            ${report.id}. Allowed values are: ${VALID_URGENCY_VALUES.join(', ')}`,
         );
       }
     });
 
     return reports;
+  }
+
+  public async getClusters(): Promise<ClusterSummary[]> {
+    const mockClusters: Array<ClusterSummary> = [
+      {
+        id: 'cluster-1-abcd',
+        isRunning: true,
+        precision: 'HIGH',
+        updatedAt: 1730233614763,
+        slackChannels: [
+          {
+            webhookUrl: 'mywebhookurl',
+            name: 'wms-dev/infra',
+            updatedAt: 1730233614763,
+          },
+        ],
+        discordChannels: [
+          {
+            webhookUrl: 'mywebhookurl',
+            name: 'wms-dev/infra',
+            updatedAt: 1730233614763,
+          },
+        ],
+        mailChannels: [
+          {
+            email: 'mail',
+            name: 'wms-dev/infra',
+            updatedAt: 1730233614763,
+          },
+        ],
+      },
+      {
+        id: 'cluster-2-abcd',
+        isRunning: false,
+        precision: 'HIGH',
+        updatedAt: 1730233614763,
+        slackChannels: [
+          {
+            webhookUrl: 'mywebhookurl',
+            name: 'wms-dev/infra',
+            updatedAt: 1730233614763,
+          },
+        ],
+        discordChannels: [
+          {
+            webhookUrl: 'mywebhookurl',
+            name: 'wms-dev/infra',
+            updatedAt: 1730233614763,
+          },
+
+          {
+            webhookUrl: 'mywebhookurl',
+            name: 'wms-dev/infra-2',
+            updatedAt: 1730233614763,
+          },
+        ],
+        mailChannels: [
+          {
+            email: 'mail',
+            name: 'wms-dev/infra',
+            updatedAt: 1730233614763,
+          },
+        ],
+      },
+    ];
+    return mockClusters;
   }
 }
 
