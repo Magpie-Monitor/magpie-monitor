39915014286c962e124faf39011ee4756086ad99
Wieslaw <al.lo3opole@gmail.com>
2024-11-02 13:22:19 +0100

    go: Add support for elasticsearch query by terms

--- go/pkg/repositories/logs.go
+++ go/pkg/repositories/logs.go
@@ -1,7 +1,7 @@
 package repositories
 
 type Log interface {
-	GetContent() string
+	GetContent() *string
 }
 
 // Split logs into subpackets of size not greater than maxPacketSize
@@ -13,15 +13,15 @@ func SplitLogsIntoPackets[T Log](logs []T, maxPacketSize int) [][]T {
 
 	for _, log := range logs {
 
-		if lastPacketSize+len(log.GetContent()) > maxPacketSize {
+		if lastPacketSize+len(*log.GetContent()) > maxPacketSize {
 
 			logsPackets = append(logsPackets, currentPacket)
 			currentPacket = []T{log}
-			lastPacketSize = len(log.GetContent())
+			lastPacketSize = len(*log.GetContent())
 
 		} else {
 			currentPacket = append(currentPacket, log)
-			lastPacketSize += len(log.GetContent())
+			lastPacketSize += len(*log.GetContent())
 		}
 
 	}
