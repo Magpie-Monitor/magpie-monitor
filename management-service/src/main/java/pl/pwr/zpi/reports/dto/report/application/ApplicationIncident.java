895fc76b26fdeadb68cbb204cc4f62dd1fc7b23d
Wojciech Suszko <wsuszko14@gmail.com>
2024-11-03 18:28:10 +0100

    management-service: Adjust contract to the new report service

--- management-service/src/main/java/pl/pwr/zpi/reports/dto/report/application/ApplicationIncident.java
+++ management-service/src/main/java/pl/pwr/zpi/reports/dto/report/application/ApplicationIncident.java
@@ -1,12 +1,16 @@
 package pl.pwr.zpi.reports.dto.report.application;
 
+import pl.pwr.zpi.reports.dto.report.Accuracy;
 import pl.pwr.zpi.reports.dto.report.Urgency;
 
 import java.util.List;
 
 public record ApplicationIncident(
         String id,
+        String title,
         String clusterId,
+        Accuracy accuracy,
+        String customPrompt,
         String applicationName,
         String category,
         String summary,
--- management-service/src/main/java/pl/pwr/zpi/reports/dto/report/node/NodeIncident.java
+++ management-service/src/main/java/pl/pwr/zpi/reports/dto/report/node/NodeIncident.java
@@ -1,11 +1,15 @@
 package pl.pwr.zpi.reports.dto.report.node;
 
+import pl.pwr.zpi.reports.dto.report.Accuracy;
 import pl.pwr.zpi.reports.dto.report.Urgency;
 
 import java.util.List;
 
 public record NodeIncident(
         String id,
+        String title,
+        Accuracy accuracy,
+        String customPrompt,
         String category,
         String clusterId,
         String nodeName,
