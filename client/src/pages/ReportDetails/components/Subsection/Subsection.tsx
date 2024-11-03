1347cac3021f44cad962f386a21e5f305a7aa90c
Wieslaw <al.lo3opole@gmail.com>
2024-11-03 18:30:52 +0100

    client: Create ReportDetails page

---
+++ client/src/pages/ReportDetails/components/Subsection/Subsection.tsx
@@ -0,0 +1,20 @@
+import 'pages/ReportDetails/ReportDetails.scss';
+
+interface ReportDetailsSubsectionProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const ReportDetailsSubsection = ({
+  children,
+  title,
+}: ReportDetailsSubsectionProps) => {
+  return (
+    <div className="report-details__subsection">
+      <div className="report-details__subsection__title">{title}</div>
+      {children}
+    </div>
+  );
+};
+
+export default ReportDetailsSubsection;
