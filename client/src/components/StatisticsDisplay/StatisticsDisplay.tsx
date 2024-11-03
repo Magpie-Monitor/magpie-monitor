a8a2e1408080cba8b90320a0c01450600c203423
Wieslaw <al.lo3opole@gmail.com>
2024-11-03 18:32:43 +0100

    client: Refactor StatisticsDisplay

---
+++ client/src/components/StatisticsDisplay/StatisticsDisplay.tsx
@@ -0,0 +1,45 @@
+import StatItem from 'components/StatItem/StatItem.tsx';
+import './StatisticsDisplay.scss';
+import UrgencyPolarChart from 'components/UrgencyPolarChart/UrgencyPolarChart';
+import { UrgencyLevel } from 'api/managment-service';
+import colors from 'global/colors';
+
+export interface StatItemData {
+    title: string;
+    value: number;
+    unit: string;
+    valueColor?: string;
+}
+
+interface StatisticsDisplayProps {
+    statItems: StatItemData[];
+    urgencyIncidentCount: Record<UrgencyLevel, number>;
+}
+
+const StatisticsDisplay = ({
+    statItems,
+    urgencyIncidentCount,
+}: StatisticsDisplayProps) => {
+    return (
+        <div className="statistics-display">
+            <div className="statistics-display__content">
+                <div className="statistics-display__items">
+                    {statItems.map((item, index) => (
+                        <StatItem
+                            key={index}
+                            title={item.title}
+                            value={item.value}
+                            unit={item.unit}
+                            valueColor={item.valueColor || colors.urgency.low}
+                        />
+                    ))}
+                </div>
+                <div className="statistics-display__chart">
+                    <UrgencyPolarChart urgencyIncidentCount={urgencyIncidentCount} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default StatisticsDisplay;
