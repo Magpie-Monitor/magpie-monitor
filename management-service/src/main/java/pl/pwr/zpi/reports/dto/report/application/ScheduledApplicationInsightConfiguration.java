a670c9ee17fbb7bc8bada1b0dbca333a8eadfeb0
Wojciech Suszko <wsuszko14@gmail.com>
2024-11-03 19:31:51 +0100

    management-service: Change accuracy to enum for scheduled node and app insights.

--- management-service/src/main/java/pl/pwr/zpi/reports/dto/report/application/ScheduledApplicationInsightConfiguration.java
+++ management-service/src/main/java/pl/pwr/zpi/reports/dto/report/application/ScheduledApplicationInsightConfiguration.java
@@ -1,8 +1,10 @@
 package pl.pwr.zpi.reports.dto.report.application;
 
+import pl.pwr.zpi.reports.dto.report.Accuracy;
+
 public record ScheduledApplicationInsightConfiguration(
         String applicationName,
-        String accuracy,
+        Accuracy accuracy,
         String customPrompt
 ) {
 }
