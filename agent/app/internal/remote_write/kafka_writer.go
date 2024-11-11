c0b6fdce1a5e0ef3ed56c98d87a5fb5e1ea532ce
Wojciech Suszko <wsuszko14@gmail.com>
2024-11-11 13:08:11 +0100

    agent: Remove buffer for testing purposes.

--- agent/app/internal/remote_write/kafka_writer.go
+++ agent/app/internal/remote_write/kafka_writer.go
@@ -32,16 +32,21 @@ func NewStreamWriter(address, topic, username, password string, batchSize int) R
 
 func (s *StreamWriter) Write(content string) {
 	msg := kafka.Message{Value: []byte(content)}
-	s.buffer = append(s.buffer, msg)
+	// s.buffer = append(s.buffer, msg)
 
-	if len(s.buffer) >= s.batchSize {
-		log.Printf("Buffer reached the batch size of: %d, sending messages.", s.batchSize)
-
-		err := s.writer.WriteMessages(context.Background(), s.buffer...)
-		if err != nil {
-			log.Printf("Error writing message: %v. Buffering, buffer size: %d.", err, len(s.buffer))
-		} else {
-			s.buffer = make([]kafka.Message, 0)
-		}
+	err := s.writer.WriteMessages(context.Background(), msg)
+	if err != nil {
+		log.Printf("Error writing message: %v. Buffering, buffer size: %d.", err, len(s.buffer))
 	}
+
+	// if len(s.buffer) >= s.batchSize {
+	// 	log.Printf("Buffer reached the batch size of: %d, sending messages.", s.batchSize)
+
+	// 	err := s.writer.WriteMessages(context.Background(), s.buffer...)
+	// 	if err != nil {
+	// 		log.Printf("Error writing message: %v. Buffering, buffer size: %d.", err, len(s.buffer))
+	// 	} else {
+	// 		s.buffer = make([]kafka.Message, 0)
+	// 	}
+	// }
 }
