241ff12c0c46cf96bea7dfc75cf4efe610fa0083
Wojciech Suszko <wsuszko14@gmail.com>
2024-11-19 16:35:25 +0100

    management-service: Email validation.

--- management-service/src/main/java/pl/pwr/zpi/notifications/email/service/EmailReceiverService.java
+++ management-service/src/main/java/pl/pwr/zpi/notifications/email/service/EmailReceiverService.java
@@ -2,6 +2,7 @@ package pl.pwr.zpi.notifications.email.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.stereotype.Service;
 import pl.pwr.zpi.notifications.email.dto.EmailReceiverDTO;
 import pl.pwr.zpi.notifications.email.entity.EmailReceiver;
@@ -15,6 +16,7 @@ import java.util.List;
 public class EmailReceiverService {
 
     private final EmailRepository emailRepository;
+    private final EmailValidator emailValidator = EmailValidator.getInstance();
 
     public List<EmailReceiver> getAllEmails() {
         return emailRepository.findAll();
@@ -33,15 +35,32 @@ public class EmailReceiverService {
         emailRepository.save(receiver);
     }
 
-    public EmailReceiver updateEmail(Long id, EmailReceiverDTO emailReceiver) {
+    public EmailReceiver updateEmail(Long id, EmailReceiverDTO updateRequest) {
         var receiver = getEmailReceiver(id);
 
-        checkIfUserCanUpdateEmail(emailReceiver.getEmail(), id);
+        checkIfUserCanUpdateEmail(updateRequest.getEmail(), id);
+        patchEmail(receiver, updateRequest);
 
-        receiver.setReceiverName(emailReceiver.getName());
-        receiver.setReceiverEmail(emailReceiver.getEmail());
-        receiver.setUpdatedAt(System.currentTimeMillis());
         return emailRepository.save(receiver);
+    }
+
+    private void patchEmail(EmailReceiver emailReceiver, EmailReceiverDTO updateRequest) {
+        if(updateRequest.getEmail() != null) {
+            validateEmail(updateRequest.getEmail());
+            emailReceiver.setReceiverEmail(updateRequest.getEmail());
+        }
+
+        if(updateRequest.getName() != null) {
+            emailReceiver.setReceiverName(updateRequest.getName());
+        }
+
+        emailReceiver.setUpdatedAt(System.currentTimeMillis());
+    }
+
+    private void validateEmail(String email) {
+        if(!emailValidator.isValid(email)) {
+            throw new RuntimeException("Invalid email");
+        }
     }
 
     public EmailReceiver getEmailReceiver(Long receiverEmailId) {
