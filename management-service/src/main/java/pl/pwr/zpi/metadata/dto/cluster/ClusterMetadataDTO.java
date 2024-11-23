c4ec54b614c0c42773d52e339d8e59ecceaf69a5
Wojciech Suszko <wsuszko14@gmail.com>
2024-11-23 10:39:02 +0100

    management-service: Add configuration metadata to clusters endpoint, refactor.
    # Conflicts:
    #	management-service/src/main/java/pl/pwr/zpi/cluster/service/ClusterService.java

--- management-service/src/main/java/pl/pwr/zpi/metadata/dto/cluster/ClusterMetadataDTO.java
+++ management-service/src/main/java/pl/pwr/zpi/metadata/dto/cluster/ClusterMetadataDTO.java
@@ -1,11 +1,21 @@
 package pl.pwr.zpi.metadata.dto.cluster;
 
+import lombok.Builder;
+import lombok.Data;
 import pl.pwr.zpi.reports.enums.Accuracy;
 
-public record ClusterMetadataDTO(
-        String clusterId,
-        //Long updatedAtMillis,
-        //Accuracy accuracy,
-        boolean running
-) {
+@Data
+@Builder
+public class ClusterMetadataDTO {
+    String clusterId;
+    Long updatedAtMillis;
+    Accuracy accuracy;
+    boolean running;
+
+    public static ClusterMetadataDTO of(String clusterId, boolean running) {
+        return ClusterMetadataDTO.builder()
+                .clusterId(clusterId)
+                .running(running)
+                .build();
+    }
 }
