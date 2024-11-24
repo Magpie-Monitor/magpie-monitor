2fc734549a13b54512017dfbea16ee24e3b20eae
XxRoloxX <al.lo3opole@gmail.com>
2024-11-24 15:22:45 +0100

    go reports-service: Fix null report sources

--- go/services/reports/pkg/insights/node_insights.go
+++ go/services/reports/pkg/insights/node_insights.go
@@ -251,11 +251,6 @@ func (g *OpenAiInsightsGenerator) GetScheduledNodeInsights(
 		return nil, err
 	}
 
-	if err != nil {
-		g.logger.Error("Failed to get application logs for scheduled insight")
-		return nil, err
-	}
-
 	insights, err := g.getNodeInsightsFromBatchEntries(completionResponses, scheduledInsights)
 	if err != nil {
 		g.logger.Error("Failed to transform batch entries into node insights")
@@ -289,11 +284,6 @@ func (g *OpenAiInsightsGenerator) AwaitScheduledNodeInsights(
 	completionResponses, err := g.client.CompletionResponseEntriesFromBatches(batches)
 	if err != nil {
 		g.logger.Error("Failed to get node batch completion responses", zap.Error(err))
-		return nil, err
-	}
-
-	if err != nil {
-		g.logger.Error("Failed to get application logs for scheduled insight")
 		return nil, err
 	}
 
