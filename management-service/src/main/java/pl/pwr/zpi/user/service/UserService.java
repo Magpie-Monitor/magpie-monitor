256144095476743c874fab21ba05fb38e602cd8e
MDybek <dybek8780@gmail.com>
2024-12-02 00:02:32 +0100

    management-service: more tests

--- management-service/src/main/java/pl/pwr/zpi/user/service/UserService.java
+++ management-service/src/main/java/pl/pwr/zpi/user/service/UserService.java
@@ -1,13 +1,10 @@
 package pl.pwr.zpi.user.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import pl.pwr.zpi.user.data.User;
 import pl.pwr.zpi.user.repository.UserRepository;
 
-import java.time.Instant;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -15,16 +12,6 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
-
-    public Optional<User> getCurrentUser() {
-        var userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        var userEmail = userDetails.getUsername();
-        return getUserByEmail(userEmail);
-    }
-
-    public Optional<User> getUserByEmail(String email) {
-        return userRepository.findByEmail(email);
-    }
 
     public User saveUser(User user) {
         return userRepository.save(user);
