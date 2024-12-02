5e39b114957af441c4af3a49213673ff291224ee
Wieslaw <al.lo3opole@gmail.com>
2024-12-02 22:34:21 +0100

    client: Create CenteredSpinner

---
+++ client/src/components/CenteredSpinner/CenteredSpinner.tsx
@@ -0,0 +1,13 @@
+import Spinner from 'components/Spinner/Spinner';
+
+import './CenteredSpinner.scss';
+
+const CenteredSpinner = () => {
+  return (
+    <div className="centered-spinner">
+      <Spinner />
+    </div>
+  );
+};
+
+export default CenteredSpinner;
