edd72d8dbb9fe022e48aa8a2d8fc2a7cfd1a7dce
Wieslaw <al.lo3opole@gmail.com>
2024-12-02 21:44:17 +0100

    client: Create usePaginatedContent hook

--- client/src/hooks/TransitionParams.tsx
+++ client/src/hooks/TransitionParams.tsx
@@ -3,5 +3,5 @@ export const FadeInTransition = {
   enter: { opacity: 1, transform: 'translateY(0)' },
   leave: { opacity: 0, transform: 'translateY(20px)' },
   config: { duration: 100 },
-  trail: 200,
+  trail: 100,
 };
