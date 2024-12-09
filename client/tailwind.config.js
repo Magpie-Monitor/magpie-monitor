a2acbf2a0285ae384f7ee817d3ac973721d2f0dd
Wieslaw <al.lo3opole@gmail.com>
2024-12-09 23:24:44 +0100

    client: Add Date Range Picker from NextUI

---
+++ client/tailwind.config.js
@@ -0,0 +1,46 @@
+import { nextui } from '@nextui-org/theme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/components/(date-picker|button|ripple|spinner|calendar|date-input|form|popover).js',
+  ],
+  // theme: {
+  //   colors: {
+  //     nextuiForeground: 'white',
+  //     nextuiBackground: '#07111b',
+  //   },
+  //   extend: {
+  //     textColor: 'white',
+  //     backgroundColor: '#07111b',
+  //   },
+  // },
+  darkMode: 'class',
+  plugins: [
+    nextui({
+      defaultTheme: 'magpie',
+      themes: {
+        magpie: {
+          extend: 'dark',
+          colors: {
+            default: {
+              50: '#122131',
+            },
+            primary: {
+              50: '#5cd06080',
+              DEFAULT: '#5cd060',
+              foreground: '#122131',
+            },
+            background: '#07111b',
+
+            content1: {
+              DEFAULT: '#07111b',
+            },
+          },
+        },
+      },
+    }),
+  ],
+};
